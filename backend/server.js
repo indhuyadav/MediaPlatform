9c058539cb0ba0af1789bb35d51d9c7fd79fa06a
indhuyadav <indhuy88@gmail.com>
2025-03-13 15:29:27 +0530

    news platform

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const newsRoutes = require("./routes/newsroute");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/news", newsRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB and Start Server
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit the process if MongoDB connection fails
+  });
